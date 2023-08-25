f4697be0e7c9f30ce292a7b806648dec1027a371
Rishil patel <rishilpatel157@gmail.com>
2023-08-25 06:14:00 +0530

    created

--- script/script.js
+++ script/script.js
@@ -109,8 +109,6 @@ window.addEventListener('scroll', scrollUp)
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'uil-sun'
-
-
 // Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
 
